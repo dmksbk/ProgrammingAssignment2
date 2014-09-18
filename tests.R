c2d1d5863fcb1a7c1efe57bb8d9209e8f06d81b3
Semenov Dmitriy <dmksbk@gmail.com>
2014-09-18 11:01:35 +0400

    Added tests

---
+++ tests.R
@@ -0,0 +1,13 @@
+library("testthat")
+
+source("cachematrix.R")
+
+context("Week 3 Programming assignment")
+
+expect_tol <- function(...) { expect_equal(tolerance=0.005, ...) }
+
+### Checking cachematrix functionality
+test_that("Cachematrix works correctly",
+{
+    
+})
